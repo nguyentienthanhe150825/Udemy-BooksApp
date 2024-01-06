0b9e0ecb2b08c2d720854ca03019e95819c17d55
NguyenThanh <nguyenthanh2542001@gmail.com>
2024-01-06 22:26:20 +0700

    Chia Layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,22 @@ import React, { useState } from 'react';
 import {
   createBrowserRouter,
   RouterProvider,
+  Outlet,
 } from "react-router-dom";
 import LoginPage from "./pages/login/index.jsx";
-
+import ContactPage from "./pages/contact/index.jsx";
+import BookPage from "./pages/book/index.jsx";
+import Header from "./components/Header/index.jsx";
+import Footer from "./components/Footer/index.jsx";
+import Home from "./components/Home/index";
 
 const Layout = () => {
   return (
-    <>
-      Main pages
-    </>
+    <div className='layout-app'>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
   )
 }
 
@@ -21,6 +28,17 @@ export default function App() {
       path: "/",
       element: <Layout />,
       errorElement: <div>404 Not Found</div>,
+      children: [
+        { index: true, element: <Home /> },
+        {
+          path: "contact",
+          element: <ContactPage />,
+        },
+        {
+          path: "book",
+          element: <BookPage />,
+        },
+      ],
     },
     {
       path: "/login",
