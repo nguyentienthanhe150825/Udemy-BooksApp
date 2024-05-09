5cebb4759fd785bb71cb98d4295d5dcc5cd731e9
NguyenThanh <nguyenthanh2542001@gmail.com>
2024-05-09 10:15:21 +0700

    complete function: loading thumbnail and slider

--- src/components/Admin/Book/BookViewDetail.jsx
+++ src/components/Admin/Book/BookViewDetail.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Drawer, Descriptions, Badge, Divider, Upload, Modal } from 'antd';
 import moment from 'moment/moment';
+import { v4 as uuidv4 } from 'uuid';
 
 const BookViewDetail = (props) => {
     const { openViewDetail, setOpenViewDetail } = props;
@@ -9,56 +10,61 @@ const BookViewDetail = (props) => {
 
     const onClose = () => {
         setOpenViewDetail(false);
-        setDataViewDetail(null);
+        // setDataViewDetail(null);
     };
 
-    const getBase64 = (file) =>
-        new Promise((resolve, reject) => {
-            const reader = new FileReader();
-            reader.readAsDataURL(file);
-            reader.onload = () => resolve(reader.result);
-            reader.onerror = (error) => reject(error);
-        });
-
+    // const getBase64 = (file) =>
+    //     new Promise((resolve, reject) => {
+    //         const reader = new FileReader();
+    //         reader.readAsDataURL(file);
+    //         reader.onload = () => resolve(reader.result);
+    //         reader.onerror = (error) => reject(error);
+    //     });
 
     const [previewOpen, setPreviewOpen] = useState(false);
     const [previewImage, setPreviewImage] = useState('');
     const [previewTitle, setPreviewTitle] = useState('');
-    const [fileList, setFileList] = useState([
-        {
-            uid: '-1',
-            name: 'image.png',
-            status: 'done',
-            url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-        },
-        {
-            uid: '-2',
-            name: 'image.png',
-            status: 'done',
-            url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-        },
-        {
-            uid: '-3',
-            name: 'image.png',
-            status: 'done',
-            url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-        },
-        {
-            uid: '-4',
-            name: 'image.png',
-            status: 'done',
-            url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+
+    const [fileList, setFileList] = useState([]);
+    //status có 3 trạng thái: done, uploading, error
+
+    //Part 5 - video 42
+    useEffect(() => {
+        if (dataViewDetail) {
+            let imgThumbnail = {};
+            let imgSlider = [];
+            if (dataViewDetail.thumbnail) {
+                imgThumbnail = {
+                    uid: uuidv4(),   //Dùng thư viện để tạo ra ID độc Nhất theo form của Upload<Antd>
+                    name: dataViewDetail.thumbnail,
+                    status: 'done',
+                    url: `${import.meta.env.VITE_BACKEND_URL}/images/book/${dataViewDetail.thumbnail}`,
+                }
+            }
+
+            if (dataViewDetail.slider && dataViewDetail.slider.length > 0) {
+                dataViewDetail.slider.map(item => {
+                    imgSlider.push({
+                        uid: uuidv4(),   //Dùng thư viện để tạo ra ID độc Nhất theo form của Upload<Antd>
+                        name: item,
+                        status: 'done',
+                        url: `${import.meta.env.VITE_BACKEND_URL}/images/book/${item}`,
+                    })
+                })
+            }
+            //Lấy imgThumbnail để lên đẩu tiên sau đó coppy lại mảng imgSlider
+            setFileList([imgThumbnail, ...imgSlider])
         }
-    ]);
+    }, [dataViewDetail])
 
 
 
-    const handleCancel = () => setPreviewOpen(false);
+
+    const handleCancel = () => {
+        setPreviewOpen(false)
+    };
 
     const handlePreview = async (file) => {
-        if (!file.url && !file.preview) {
-            file.preview = await getBase64(file.originFileObj);
-        }
         setPreviewImage(file.url || (file.preview));
         setPreviewOpen(true);
         setPreviewTitle(file.name || file.url.substring(file.url.lastIndexOf('/') + 1));
